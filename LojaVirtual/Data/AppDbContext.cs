51ed77f281875bc946f1451a4e96f36f60891fb5
Mate Ferreira <mateusaf.gamedev@gmail.com>
2026-08-31 10:58:13 -0300

    Subindo container no docker + criação da comunicação com o banco + adicionando migrações

--- LojaVirtual/Data/AppDbContext.cs
+++ LojaVirtual/Data/AppDbContext.cs
@@ -4,5 +4,11 @@ namespace LojaVirtual.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        // Define your DbSets here, for example:
+        // public DbSet<Product> Products { get; set; }
     }
 }
--- LojaVirtual/Program.cs
+++ LojaVirtual/Program.cs
@@ -1,7 +1,16 @@
+using LojaVirtual.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDockerConnection"))
+);
+
 
 var app = builder.Build();
 
