81843d9f9ae51a2c913d8cfc2c056e975141441a
Mate Ferreira <mateusaf.gamedev@gmail.com>
2026-09-14 10:43:13 -0300

    Criação de dto para cadastro de produtos

---
+++ LojaVirtual/DTO/ProdutoDto/CriarProdutoDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaVirtual.DTO.ProdutoDto
+{
+    public class ProdutoCriarDto
+    {
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Foto { get; set; }
+        public double Valor { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
